455dff2d570f22c55ff1344b8c83867642d5bc2e
sangminLee <nalsm98@naver.com>
2020-01-18 13:53:58 +0900

    CategoryListOrAddController 컨트롤러 클래스 수정 * URL 수정 * 구현사항 추가

--- minsblog/src/controls/CategoryListOrAddController.java
+++ minsblog/src/controls/CategoryListOrAddController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component("/category.do")
+@Component("/category/list.do")
 public class CategoryListOrAddController implements DataBinding, Controller {
 
   PostgresSqlCategoryDao categoryDao;
@@ -21,7 +21,10 @@ public class CategoryListOrAddController implements DataBinding, Controller {
   @Override
   public String execute(Map<String, Object> model) throws Exception {
     HashMap<String, Object> paramMap = new HashMap<>();
+    // TODO session 에서 어드민 정보가 있는지 확인 필요
     HttpSession session = (HttpSession) model.get("session");
+    // TODO 카테고리가 Main 일때와 아닐때로 구분 필요
+    // TODO Post mapper 파일에 카테고리 id로 부터 해당 글을 불러오는 조인 쿼리 추가 해야함
     // 아직은 paramMap이 사용되지는 않음
     model.put("category", categoryDao.selectList(paramMap));
     return "/home/home.jsp";
