51802c531c44c117b129f7be3de27e37f3aa704a
sangminLee <nalsm98@naver.com>
2020-01-18 15:24:52 +0900

    카테고리 수정 컨트롤러 구현 완료

---
+++ minsblog/src/controls/CategoryUpdateController.java
@@ -0,0 +1,37 @@
+package controls;
+
+import annotation.Component;
+import bind.DataBinding;
+import dao.PostgresSqlCategoryDao;
+import vo.Category;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component("/category/update.do")
+public class CategoryUpdateController implements DataBinding, Controller {
+
+  PostgresSqlCategoryDao categoryDao;
+
+  public void setCategoryDao(PostgresSqlCategoryDao categoryDao) {
+    this.categoryDao = categoryDao;
+  }
+
+  @Override
+  public Object[] getDataBinders() {
+    return new Object[] {
+        "category", Category.class,
+        "beforeName", String.class,
+        "changeName", String.class
+    };
+  }
+  
+  @Override
+  public String execute(Map<String, Object> model) throws Exception {
+    HashMap<String, Object> paramMap = new HashMap<>();
+    paramMap.put("beforeName", model.get("beforeName"));
+    paramMap.put("changeName", model.get("changeName"));
+    categoryDao.update(paramMap);
+    return "redirect:/category/list.do?cid=Main";
+  }
+}
