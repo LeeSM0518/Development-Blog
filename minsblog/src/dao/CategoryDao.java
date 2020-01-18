98e752d3a4e1df12e8453425a43d5fd5cb0c02a7
sangminLee <nalsm98@naver.com>
2020-01-18 15:19:34 +0900

    CategoryDao 업데이트 * delete 메소드의 매개변수 Category로 변경 * update 메소드의 매개변수 HashMap으로 변경

--- minsblog/src/dao/CategoryDao.java
+++ minsblog/src/dao/CategoryDao.java
@@ -9,8 +9,8 @@ public interface CategoryDao {
 
   List<Category> selectList(HashMap<String, Object> paramMap) throws Exception;
   int insert(Category category) throws Exception;
-  int delete(int id) throws Exception;
+  int delete(Category category) throws Exception;
   Category selectOne(int id) throws Exception;
-  int update(Category category) throws Exception;
+  int update(HashMap<String, Object> paramMap) throws Exception;
 
 }
